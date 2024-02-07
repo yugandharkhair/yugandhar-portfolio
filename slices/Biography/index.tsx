d7e240cd30a715366cf34ef8922e81d71101824e
Yugandhar Khair <yugkhair@gmail.com>
2024-02-06 16:33:38 -0800

    Final Site

---
+++ slices/Biography/index.tsx
@@ -0,0 +1,38 @@
+import Button from "@/app/components/Button";
+import Heading from "@/app/components/Heading";
+import Bounded from "@/app/components/bounded";
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import Avatar from "./Avatar";
+
+/**
+ * Props for `Biography`.
+ */
+export type BiographyProps = SliceComponentProps<Content.BiographySlice>;
+
+/**
+ * Component for "Biography" Slices.
+ */
+const Biography = ({ slice }: BiographyProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="grid gap-x-8 gap-y-6 grid-cols-[2fr, 1fr]">
+        <Heading as="h1" size="xl" className="col-start-1">
+          {slice.primary.heading}
+        </Heading>
+        <div className="prose prose-xl prose-slate prose-invert col-start-1">
+          <PrismicRichText field={slice.primary.description}/>
+        </div>
+        <Button label={slice.primary.button_text} linkField={slice.primary.button_link}/>
+
+        <Avatar image={slice.primary.avatar} className="row-start-1 max-w-sm md:col-start-2 md: row-end-3"/>
+      </div>
+    </Bounded>
+  );
+};
+
+export default Biography;
